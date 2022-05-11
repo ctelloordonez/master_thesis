5daeb3066c7ac37f619f52a9dec773550f25f3e1
Carlos Tello Ordoñez <charlietello97@gmail.com>
2022-05-11 12:27:34 +0200

    changed X axis to percentaje

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 from utils import ASCII_to_dict
 
@@ -6,9 +7,17 @@ from utils import ASCII_to_dict
 def main():
     participant = ASCII_to_dict()
 
-    x = participant['V4']['R']['Angles']['T5']['St2_Knee_X']
+    init_value = 0
+    fin_value = 1001
+    no_samples = np.arange(init_value, fin_value)
+    m = (100-0) / (fin_value - init_value)
+    b = 0 - m * init_value
+    new_x = no_samples * m + b
+
+    y = participant['V4']['R']['Angles']['T5']['St2_Knee_X']
+
     fig, ax = plt.subplots()
-    ax.plot(range(0, 1001), x)
+    ax.plot(new_x, y)
     ax.set(xlabel='Gait Cycle %', ylabel='Sagittal Knee Joint Angle [Degrees]',
            title='V4 R Knee angles')
     plt.savefig('data/plots/example_plot.png')
